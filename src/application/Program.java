0d9ec8a59586b6bf43f825282884c4921f69a79f
Creating project and git repository <lucasbonnet688@gmail.com>
2025-05-23 10:33:52 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,13 +15,21 @@ public class Program {
 		Seller seller = sellerDao.findById(2);
 		
 		System.out.println(seller);
+		
 		System.out.println("===========================================");
+		
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
 		
+		System.out.println("===========================================");
+		
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 
 	}
 
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -73,8 +73,41 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement("SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ " ON seller.DepartmentId = department.Id "
+					+ "ORDER BY Name");
+				
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<Seller>();
+			
+			Map<Integer, Department> map = new HashMap<Integer, Department>();
+			
+			while(rs.next()) {
+				
+				Department depp = map.get(rs.getInt("DepartmentId"));
+				
+				if(depp==null) {
+					depp = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), depp);
+				}
+				Seller sel = instantiateSeller(rs, depp);
+				list.add(sel);
+			}
+			return list;
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 	
 	@Override
